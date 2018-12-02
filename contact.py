ca4aa70c9eb220ca92f3889e3a2ad66e1419ebc8
Andrey Mukhin <mukhinaa89@yandex.ru>
2018-12-02 20:57:11 +0500

    Для контактов. Выделены вспомогательные методы и классы.

---
+++ contact.py
@@ -0,0 +1,20 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone,
+                 mobile_phone, work_phone, fax, email, birth_day, birth_month, birth_year):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
--- test_add_contact.py
+++ test_add_contact.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from contact import Contact
 
 class AddContact(unittest.TestCase):
     def setUp(self):
@@ -17,8 +18,8 @@ class AddContact(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         # init contact creation
         wd.find_element_by_link_text("add new").click()
-        self.create_contact(wd, firstname="first", middlename="middle", lastname="last", nickname="nick", title="title", company="company", address="address1", home_phone="7123714", mobile_phone="123134",
-                            work_phone="23423423", fax="234234", email="email@email.ru", birth_day="16", birth_month="January", birth_year="1989")
+        self.create_contact(wd, Contact(firstname="first", middlename="middle", lastname="last", nickname="nick", title="title", company="company", address="address1", home_phone="7123714", mobile_phone="123134",
+                            work_phone="23423423", fax="234234", email="email@email.ru", birth_day="16", birth_month="January", birth_year="1989"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -29,8 +30,8 @@ class AddContact(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         # init contact creation
         wd.find_element_by_link_text("add new").click()
-        self.create_contact(wd, firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="", mobile_phone="",
-                            work_phone="", fax="", email="", birth_day="", birth_month="-", birth_year="")
+        self.create_contact(wd, Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home_phone="", mobile_phone="",
+                            work_phone="", fax="", email="", birth_day="", birth_month="-", birth_year=""))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -41,40 +42,39 @@ class AddContact(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_contact(self, wd, firstname, middlename, lastname, nickname, title, company, address, home_phone,
-                       mobile_phone, work_phone, fax, email, birth_day, birth_month, birth_year):
+    def create_contact(self, wd, contact):
         # add Name (first, middle,last) and Nickname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(contact.title)
         # add Company's information
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         # add Phones and Email
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home_phone)
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile_phone)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work_phone)
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(fax)
+        wd.find_element_by_name("fax").send_keys(contact.fax)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(contact.email)
         # add Date of birth
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(birth_day)
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(birth_month)
-        wd.find_element_by_name("byear").send_keys(birth_year)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birth_day)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birth_month)
+        wd.find_element_by_name("byear").send_keys(contact.birth_year)
         # submit contact creation
         wd.find_element_by_xpath("//input[21]").click()
 
