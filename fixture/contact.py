4005a9a3c07ce04a28a9ef181ccfe4c5e13f3744
Andrey Mukhin <mukhinaa89@yandex.ru>
2018-12-21 05:44:52 +0500

    Задание №7: Сделать все тесты создания, удаления, модификации для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -46,3 +46,12 @@ class ContactHelper:
         # submit contact creation
         wd.find_element_by_xpath("//input[21]").click()
         self.return_to_home_page()
+
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        #select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
