6c117904a6b693421821766f73fa12ec8ca0d57b
Andrey Mukhin <mukhinaa89@yandex.ru>
2018-12-23 23:44:59 +0500

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий: для модификации контактов и групп.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_first_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(nickname="test"))
     app.contact.modify_first_contact(Contact(firstname="Vasiliy"))
 
 
 def test_modify_last_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(address="Moscow"))
     app.contact.modify_first_contact(Contact(lastname="Alibabaevich"))
