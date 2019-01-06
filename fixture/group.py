8092d80c5c79557d35378b720ccfbad752289157
Andrey Mukhin <mukhinaa89@yandex.ru>
2019-01-07 04:11:18 +0500

    Добавляем немного случайности в тесты (для модификации групп)

--- fixture/group.py
+++ fixture/group.py
@@ -61,11 +61,16 @@ class GroupHelper:
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+        
 
-    def modify_first_group(self, new_group_data):
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
