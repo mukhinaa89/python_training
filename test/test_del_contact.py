25067481c22cb48524aa64415f7bfb9e3a33e464
Andrey Mukhin <mukhinaa89@yandex.ru>
2019-02-19 21:43:52 +0500

    Задание 20. Подправил косяки v.3.0

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -12,6 +12,8 @@ def test_delete_some_contact(app, db, check_ui):
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    def clean(contact):
+        return Contact(id=contact.id, lastname=contact.lastname.strip(), firstname=contact.firstname.strip())
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
-                                                                     key=Contact.id_or_max)
+        assert sorted(map(clean, new_contacts), key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                                 key=Contact.id_or_max)
