7960d1b67bda901ddf5576c9f114dd33b8f6f0a0
Andrey Mukhin <mukhinaa89@yandex.ru>
2019-02-21 05:03:00 +0500

    Задание №20: Переделать тесты, чтобы сравнивались данные, загруженные из БД v 2.0

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -9,11 +9,11 @@ def test_delete_some_contact(app, db, check_ui):
     contact = random.choice(old_contacts)
     app.contact.delete_contact_by_id(contact.id)
     new_contacts = db.get_contact_list()
-    # assert len(old_contacts) - 1 == len(new_contacts)
+    assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
-    def clean(contact):
-        return Contact(id=contact.id, lastname=contact.lastname.strip(), firstname=contact.firstname.strip())
+    # def clean(contact):
+    #     return Contact(id=contact.id, lastname=contact.lastname.strip(), firstname=contact.firstname.strip())
     if check_ui:
-        assert sorted(map(clean, new_contacts), key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                                  key=Contact.id_or_max)
